6e45d9051f852017209d5e56b3b0427198ecbb9b
Saulo Rodrigues <130412074+SauloRodrigues20@users.noreply.github.com>
2025-12-11 15:58:32 +0000

    fix: adicionar @OptIn para APIs experimentais do Material3

--- app/src/main/java/com/agrogeocolector/MainActivity.kt
+++ app/src/main/java/com/agrogeocolector/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.agrogeocolector.ui.map.MapLibreScreen
